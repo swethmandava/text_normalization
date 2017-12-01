a6863600fd4a8429863094f39ff0c4286b36c694
Swetha Mandava <sweth.mandava@gmail.com>
2017-11-30 23:58:25 -0500

    full model - text parsing

--- blstm_new.py
+++ blstm_new.py
@@ -6,8 +6,54 @@ import numpy as np
 path = ''
 data_exists = 1
 USE_GPU = 0 
-is_train = True   
-   
+is_train = True 
+
+def initialize_all_variables(sess=None):
+	# Credit : yaroslavvb
+	"""Initializes all uninitialized variables in correct order. Initializers
+	are only run for uninitialized variables, so it's safe to run this multiple
+	times.
+	Args:
+	  sess: session to use. Use default session if None.
+	"""
+
+	from tensorflow.contrib import graph_editor as ge
+	def make_initializer(var): 
+		def f():
+			return tf.assign(var, var.initial_value).op
+		return f
+
+	def make_noop(): return tf.no_op()
+
+	def make_safe_initializer(var):
+		"""Returns initializer op that only runs for uninitialized ops."""
+		return tf.cond(tf.is_variable_initialized(var), make_noop,
+			make_initializer(var), name="safe_init_"+var.op.name).op
+
+	if not sess:
+		sess = tf.get_default_session()
+	g = tf.get_default_graph()
+	  
+	safe_initializers = {}
+	for v in tf.all_variables():
+		safe_initializers[v.op.name] = make_safe_initializer(v)
+	  
+	# initializers access variable vaue through read-only value cached in
+	# <varname>/read, so add control dependency to trigger safe_initializer
+	# on read access
+	for v in tf.all_variables():
+		var_name = v.op.name
+		var_cache = g.get_operation_by_name(var_name+"/read")
+		ge.reroute.add_control_inputs(var_cache, [safe_initializers[var_name]])
+
+	sess.run(tf.group(*safe_initializers.values()))
+
+	# remove initializer dependencies to avoid slowing down future variable reads
+	for v in tf.all_variables():
+		var_name = v.op.name
+		var_cache = g.get_operation_by_name(var_name+"/read")
+		ge.reroute.remove_control_inputs(var_cache, [safe_initializers[var_name]]) 
+
 def create_data() :
    
 	#load X , Y , len_x , len_y
@@ -50,8 +96,8 @@ def BiRNN(num_hidden, num_classes, learning_rate, encoding_layers, vocab_size,
 	#embedding_encoder --> embedding matrix
 	
 	embedding_encoder = tf.random_normal((num_classes,300 ))
-	# decoding_encoder = tf.one_hot(vocab_size, vocab_size, dtype=tf.float32)
-	#decoding_encoder = tf.random_normal(( vocab_size, vocab_size))
+	#decoding_encoder = tf.one_hot(vocab_size, vocab_size, dtype=tf.float32)
+	decoding_encoder = tf.random_normal(( vocab_size, vocab_size))
 	#@TODO Gotta define encodings
 	#embedding_encoder = tf.get_variable("embeddings", shape=embedding_matrix.shape,  \
 	#                          initializer=tf.constant_initializer(np.array(embedding_matrix)) , trainable=True)  
@@ -59,9 +105,9 @@ def BiRNN(num_hidden, num_classes, learning_rate, encoding_layers, vocab_size,
 	x_input = tf.placeholder(tf.int32, [None, max_in_time])
 	X = tf.nn.embedding_lookup(embedding_encoder, x_input)
 	X_length = tf.placeholder(tf.int32, [None])
-	y_input = tf.placeholder(tf.float32, [None, max_out_time])
+	y_input = tf.placeholder(tf.int32, [None, max_out_time])
 	y_shifted_input = tf.placeholder(tf.int32, [None, max_out_time])
-	#Y = tf.nn.embedding_lookup(decoding_encoder, y_input) #### do we need this ###
+	Y = tf.nn.embedding_lookup(decoding_encoder, y_input) #### do we need this ###
 	# start_token = tf.nn.embedding_lookup(decoding_encoder, start_token)
 	# end_token = tf.nn.embedding_lookup(decoding_encoder, end_token)
 	# print start_token
@@ -100,23 +146,39 @@ def BiRNN(num_hidden, num_classes, learning_rate, encoding_layers, vocab_size,
 		# Backward direction stacked lstm cell
 		lstm_bw_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.LSTMCell(num_hidden) for _ in range(encoding_layers)])
 
-	encoder_outputs, encoder_state = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, X, 
-			sequence_length=X_length, dtype=tf.float32)
-	encoder_outputs = tf.concat(encoder_outputs, 2)
-	# encoder_state = tf.concat(encoder_state, 2)
+	((encoder_fw_outputs, encoder_bw_outputs), (encoder_fw_state, encoder_bw_state)) = \
+	tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, X, sequence_length=X_length, dtype=tf.float32)
+	
+	encoder_outputs = tf.concat((encoder_fw_outputs, encoder_bw_outputs), 1)
+	if isinstance(encoder_fw_state, rnn.LSTMStateTuple):  # LstmCell
+		state_c = tf.concat(
+			(encoder_fw_state.c, encoder_bw_state.c), 1)
+		state_h = tf.concat(
+			(encoder_fw_state.h, encoder_bw_state.h), 1)
+		encoder_state = rnn.LSTMStateTuple(c=state_c, h=state_h)
+	elif isinstance(encoder_fw_state, tuple) \
+			and isinstance(encoder_fw_state[0], rnn.LSTMStateTuple):  # MultiLstmCell
+		encoder_state = tuple(map(
+			lambda fw_state, bw_state: rnn.LSTMStateTuple(
+				c=tf.concat((fw_state.c, bw_state.c), 1),
+				h=tf.concat((fw_state.h, bw_state.h), 1)),
+			encoder_fw_state, encoder_bw_state))
+	else:
+		encoder_state = tf.concat(
+			(encoder_fw_state, encoder_bw_state), 1)
 
 	if USE_GPU :
 		 cells = []
 		 for i in range(decoding_layers):
 			 cells.append(tf.contrib.rnn.DeviceWrapper(
-					 tf.contrib.rnn.LSTMCell(num_hidden),
+					 tf.contrib.rnn.LSTMCell(2*num_hidden),
 					 "/gpu:%d" % (decoding_layers % num_gpus)))
 	
 		 decoder_cell = tf.nn.rnn_cell.MultiRNNCell(cells)
 	else :    
-		 decoder_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.LSTMCell(num_hidden) for _ in range(decoding_layers)])
+		 decoder_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.LSTMCell(2*num_hidden) for _ in range(decoding_layers)])
 	
-	
+	print "ENCODER DONE"	
 	
 	
 	projection_layer = tf.layers.Dense(vocab_size)  ## linear ---> Wx + b  
@@ -124,7 +186,7 @@ def BiRNN(num_hidden, num_classes, learning_rate, encoding_layers, vocab_size,
 	#Size is [batch_size, max_time, num_units]
 
 	attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(2*num_hidden, 
-		attention_states, memory_sequence_length=X_length)
+		attention_states) #, memory_sequence_length=X_length)
 
 	# decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, 
 	# 	attention_mechanism, attention_layer_size=2*num_hidden)
@@ -133,17 +195,18 @@ def BiRNN(num_hidden, num_classes, learning_rate, encoding_layers, vocab_size,
 		attention_mechanism, attention_layer_size=2*num_hidden)
 	decoder_cell = tf.contrib.rnn.OutputProjectionWrapper(attn_cell, vocab_size)
 	
-	# initial_state = tf.zeros([batch_size, num_hidden])
 	if is_train:   
-		helper = tf.contrib.seq2seq.TrainingHelper(y_input, Y_length)
+		print "DECODING"
+		helper = tf.contrib.seq2seq.TrainingHelper(Y, Y_length)
 		decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, helper, 
+			# initial_state=decoder_cell.zero_state(dtype=tf.float32, batch_size=batch_size).clone(cell_state=encoder_state), output_layer=projection_layer)
 			initial_state=decoder_cell.zero_state(dtype=tf.float32, batch_size=batch_size), output_layer=projection_layer)
 		output, _, output_lengths = tf.contrib.seq2seq.dynamic_decode(decoder, maximum_iterations=max_out_time)
 
 	else: # Eval
 		decoder = tf.contrib.seq2seq.BeamSearchDecoder(
 		cell=decoder_cell,
-		embedding = decoding_encoder,
+		# embedding = decoding_encoder,
 		start_tokens= [start_token]*batch_size,
 		end_token= end_token,
 		initial_state=decoder_cell.zero_state(dtype=tf.float32, batch_size=batch_size),
@@ -211,7 +274,8 @@ if __name__ == '__main__':
 
 	tf.reset_default_graph()
 	# Initialize the variables (i.e. assign their default value)
-	init = tf.global_variables_initializer()
+	# init = tf.global_variables_initializer()
+	# init = tf.initialize_all_variables()
 
 	# Save checkpoints on the way
 	###saver = tf.train.Saver() @ERROR
@@ -225,20 +289,25 @@ if __name__ == '__main__':
 	
 	# Start training
 	with tf.Session() as sess:
+		global is_train
+		is_train = True
+
+		print "TRAINING"
 		x_input, y_input, y_shifted_input, X_length, Y_length, logits, loss_op, optimizer = \
 		BiRNN(num_hidden, num_classes, learning_rate, encoding_layers, vocab_size , \
 			  decoding_layers, max_in_time, max_out_time, beam_width, start_token, end_token)
 		# Run the initializer
 		# sess.run(init)
+		initialize_all_variables()
+		print sess.run(tf.report_uninitialized_variables())
+		print "KILL ME"
 		# writer = tf.summary.FileWriter("logs/", graph=tf.get_default_graph())
-		global is_train
-		is_train = True
-
+		print "INITIALIZED"
 		for epoch in range(1, epochs+1):
-			for step in range(batch_size): 
+			for step in range(num_batches): 
 
 				batch_x = train_x[step * batch_size: (step+1)*batch_size, :]
-				batch_y = train_y[step * batch_size: (step+1)*batch_size, :]
+				batch_y_shifted = train_y[step * batch_size: (step+1)*batch_size, :]
 				x_length = len_x[step * batch_size: (step+1)*batch_size]
 				y_length = len_y[step * batch_size: (step+1)*batch_size]
 
@@ -246,7 +315,9 @@ if __name__ == '__main__':
 				# Define Y as timesteps_output x num_input. Include start and end tags for all
 				# I "think" only end tags will also do if we don't have to learn embeddings.
 
-				batch_y_shifted = np.hstack((start_token * np.ones((batch_size, 1)), batch_y[:, :-1]))
+				batch_y = np.hstack((start_token * np.ones((batch_y_shifted.shape[0], 1)), batch_y_shifted[:, :-1]))
+				# print batch_y, batch_y_shifted
+				# batch_y = np.copy(batch_y)
 				feed_dict = {x_input:batch_x, X_length:x_length, y_input:batch_y, Y_length:y_length,
 							 y_shifted_input:batch_y_shifted}
 				#Run and train
@@ -264,12 +335,11 @@ if __name__ == '__main__':
 		global is_train
 		is_train = False
 		# valid_x, valid_x_length, valid_y, valid_y_length = validation_data
-		valid_x, valid_x_length, valid_y, valid_y_length = create_data()
-		valid_size = valid_y.shape[0]
-		valid_y_shifted = np.hstack((start_token * np.ones((valid_size, 1)), valid_y[:, :-1]))
+		valid_x, valid_x_length, valid_y_shifted, valid_y_length = create_data()
+		valid_y = np.hstack((start_token * np.ones((valid_y_shifted.shape[0], 1)), valid_y_shifted[:, :-1]))
 
 		feed_dict = {x_input:valid_x, X_length:valid_x_length, y_input:valid_y, 
 					Y_length:y_length, y_shifted_input:valid_y_shifted}
-		sample_id, loss_op = sess.run([sample_id, loss_op], feed_dict)
+		logits, loss_op = sess.run([logits, loss_op], feed_dict)
 
 		print "Validation loss is:", loss_op
